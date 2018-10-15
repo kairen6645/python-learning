e926e7cf131fcb0af66a411632cab4ba755cb3b5
kairen6645 <kairen6645@163.com>
2018-10-15 15:08:13 +0800

    1. C# invoke python，using pythonnet

---
+++ csharp_invokePython/pythonnet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+
+
+namespace testpythonnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PythonEngine.Initialize();
+            using (Py.GIL())
+            {
+                //Creating module object
+                PyObject testClassModule = PythonEngine.ImportModule("Iron_hello");
+
+                //Calling module method
+                PyObject result = testClassModule.InvokeMethod("test");
+                Console.WriteLine("Test method result = {0}", result.ToString());
+
+                //dynamic classTest = testClassModule.GetAttr("ClassTest");
+                //dynamic tmp = classTest();
+                //tmp.Initalize();
+                //tmp.Execute();
+            }
+            PythonEngine.Shutdown();
+        }
+
+
+    }
+}
